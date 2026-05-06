623e445f93d4ed5bbdb17358e0fc2a88e263d72c
Edmon Nascimento Santos <edmon.santos@hge01.ba>
2026-05-06 12:00:54 -0300

    feat: create counter

--- Passenger counter app/index.js
+++ Passenger counter app/index.js
@@ -1,2 +1,9 @@
-//document.getElementById("count-el").innerText = 5
+let count = 0
+const countTxt = document.getElementById("count-el")
 
+const increamentBtn = document.getElementById("increment-btn")
+
+function incrementCount(){
+    count++
+    countTxt.innerText = count
+}
